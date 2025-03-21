8805db62af67d9ed9e753e158f3c602c4c9f455c
ade123a <bashirniyi@gmail.com>
2025-03-21 08:25:52 -0600

    Added router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+      <header>
+        <img src="logo.jpg" alt="LMS Logo" style="width: 100px; height: 100px" />
+        <h1>LMS - Learning Management System</h1>
       </header>
+
+      <BrowserRouter>
+      <Route path="/home" compoment={Home} />
+      <Route path="/courses" compoment={Courses} />
+      <Route path="/login" compoment={login} />
+      
+      </BrowserRouter>
+
+
     </div>
   );
 }
